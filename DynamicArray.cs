29fc0c31e22c25d5a49f9c5b2f9ec1bf08b24df1
Emir Ćedić <45953377+emircedic@users.noreply.github.com>
2023-06-20 20:03:14 +0200

    Initial setup and added DynamicArray.

---
+++ DynamicArray.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+
+namespace DataStructures
+{
+    public class DynamicArray<T> : IEnumerable<T>
+    {
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+        public T[] Values { get; set; }
+
+        public DynamicArray(int capacity = 16)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            Count = 0;
+            Capacity = capacity;
+            Values = new T[capacity];
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Values[i] = default(T);
+            }
+
+            Count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (Count + 1 >= Capacity)
+            {
+                // Extend array
+
+                // Double the capacity
+                Capacity *= 2;
+                var extendedArray = new T[Capacity];
+                for (int i = 0; i < Count; i++)
+                {
+                    extendedArray[i] = Values[i];
+                }
+
+                Values = extendedArray;
+            }
+
+            // Add item
+            Values[Count++] = item;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                Values[i] = Values[i + 1];
+            }
+
+            Count--;
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Values[i].Equals(item))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (Values[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            return IndexOf(item) == -1 ? false : true;
+        }
+
+        public override string ToString()
+        {
+            var result = "";
+
+            for (int i = 0; i < Count; i++)
+            {
+                result += Values[i];
+            }
+
+            return result;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new DynamicArrayEnumerator<T>(Values, Count);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
+public class DynamicArrayEnumerator<T> : IEnumerator<T>
+{
+    private T[] _values;
+    private int _count;
+    private int _currentIndex;
+
+    public DynamicArrayEnumerator(T[] values, int count)
+    {
+        _values = values;
+        _count = count;
+        _currentIndex = -1;
+    }
+
+    public T Current => _values[_currentIndex];
+
+    object IEnumerator.Current => Current;
+
+    public void Dispose()
+    {
+    }
+
+    public bool MoveNext()
+    {
+        _currentIndex++;
+        return _currentIndex < _count;
+    }
+
+    public void Reset()
+    {
+        _currentIndex = -1;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures;
+
+Console.WriteLine("Hello, World!");
+
+#region Dynamic Array
+
+var dynamicArray = new DynamicArray<int>();
+dynamicArray.Add(0);
+dynamicArray.Add(1);
+dynamicArray.Add(2);
+dynamicArray.Add(3);
+dynamicArray.Add(4);
+dynamicArray.Add(5);
+dynamicArray.Add(6);
+
+
+foreach (var item in dynamicArray)
+{
+    Console.WriteLine(item);
+}
+
+var stringResult = dynamicArray.ToString();
+
+dynamicArray.RemoveAt(2);
+dynamicArray.Remove(3);
+var contains = dynamicArray.Contains(4);
+var index = dynamicArray.IndexOf(1);
+dynamicArray.Clear();
+
+#endregion
+
+Console.WriteLine("Hello, World!");
