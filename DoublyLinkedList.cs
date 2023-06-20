188b1ec5f7d5db9e8c693af44d45cf807cbc8d44
Emir Ćedić <45953377+emircedic@users.noreply.github.com>
2023-06-20 22:00:37 +0200

    Added DoublyLinkedList specification.

---
+++ DoublyLinkedList.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+
+namespace DataStructures
+{
+    public class DoublyLinkedList<T> : IEnumerable<T>
+    {
+        public void Clear()
+        {
+
+        }
+
+        public int Size()
+        {
+        }
+
+        public bool IsEmpty()
+        {
+
+        }
+
+        public void Add(T item)
+        {
+
+        }
+
+        public void AddLast(T item)
+        {
+
+        }
+
+        public void AddFirst(T item)
+        {
+
+        }
+
+        public T PeekFirst()
+        {
+
+        }
+
+        public T PeekLast()
+        {
+
+        }
+
+        public T RemoveFirst()
+        {
+
+        }
+
+        public T RemoveLast()
+        {
+
+        }
+
+        public T Remove(Node<T> node)
+        {
+
+        }
+
+        public T RemoveAt(int index)
+        {
+
+        }
+
+        public bool Remove(T value)
+        {
+
+        }
+
+        public int IndexOf(T value)
+        {
+
+        }
+
+        public bool Contains(T value)
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Node<T>
+    {
+
+    }
+
+    // Todo: Add custom iterator
+}
