a2f1da04f9fdd6dc67ebd3e251d5cab88251de5d
Emir Ćedić <45953377+emircedic@users.noreply.github.com>
2025-06-10 23:42:23 +0200

    Finished defining remaining data structures.

--- Hash.cs
+++ Hash.cs
@@ -1,6 +1,159 @@
 ﻿namespace DataStructures
 {
-    public class HashSet {}
+    // Look up other collision handling techniques: https://darsh-patel.hashnode.dev/collision-handling-techniques-in-hashing-you-need-to-know
 
-    public class HashMap {}
+    public class Node
+    {
+        public int Key { get; set; }
+        public int Value { get; set; }
+
+        public Node(int key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+    public class HashMap
+    {
+        private int _capacity { get; set; }
+        private int _size { get; set; }
+        private Node[] _hashMap { get; set; }
+
+        private int Hash(int key)
+        {
+            return key.GetHashCode() % _capacity;
+        }
+
+        public HashMap(int capacity)
+        {
+            _capacity = capacity;
+            _size = 0;
+            _hashMap = new Node[_capacity];
+        }
+
+        public void Insert(int key, int value)
+        {
+            var hashValue = Hash(key);
+
+            // There are three options while trying to add a new value:
+            while (true)
+            {
+                // 1. The specified hash value location is free.
+                if (_hashMap[hashValue] == null)
+                {
+                    // Insert new value.
+                    var node = new Node(key, value);
+                    _hashMap[hashValue] = node;
+
+                    _size++;
+
+                    // Check if the hash map should be resized.
+                    if (_size >= (_capacity / 2))
+                        Resize();
+
+                    return;
+                }
+
+                // 2. The specified hash value is occupied with the same key.
+                else if (_hashMap[hashValue].Key == key)
+                {
+                    // Update the existing entry with new value.
+                    _hashMap[hashValue].Value = value;
+                    return;
+                }
+
+                // 3. The specified hash value is occupied with a different key.
+                else if (_hashMap[hashValue].Key != key)
+                {
+                    // Try to insert at the next position but be careful of exceeding the max capacity.
+                    hashValue = (hashValue + 1) % _capacity;
+                    continue;
+                }
+            }
+        }
+
+        public int Get(int key)
+        {
+            var hashValue = Hash(key);
+
+            // While retrieving a value from hash map, there are three possible options:
+            while (true)
+            {
+                // 1. The location specified by hash has no value which means the specified entry does not exist.
+                if (_hashMap[hashValue] == null)
+                    break;
+
+                // 2. The location specified by hash has a different value than the key.
+                else if (_hashMap[hashValue].Key != key)
+                {
+                    hashValue = (hashValue + 1) % _capacity;
+                    continue;
+                }
+
+                // 3. The location specified by hash has the same value as the key.
+                else if (_hashMap[hashValue].Key == key)
+                    return _hashMap[hashValue].Value;
+            }
+
+            return -1;
+        }
+
+        public bool Remove(int key)
+        {
+
+            var hashValue = Hash(key);
+
+            while (true)
+            {
+                if (_hashMap[hashValue] == null)
+                    return false;
+                else if (_hashMap[hashValue].Key != key)
+                {
+                    hashValue = (hashValue + 1) % _capacity;
+                    continue;
+                }
+                else if (_hashMap[hashValue].Key == key)
+                {
+                    _hashMap[hashValue] = null;
+                    _size--;
+                    return true;
+                }
+            }
+        }
+
+        public int GetSize()
+        {
+            return _size;
+        }
+
+        public int GetCapacity()
+        {
+            return _capacity;
+        }
+
+        public void Resize()
+        {
+            // Update capacity.
+            _capacity *= 2;
+
+            // Create a copy of the old hash map and set its old instance to a new hash map with double the capacity.
+            Node[] oldHashMap = _hashMap;
+            _hashMap = new Node[_capacity];
+
+            // Set the size to 0 and increment it while re-adding the existing hash map values.
+            _size = 0;
+
+            // Iterate through old hash map.
+            foreach (Node node in oldHashMap)
+            {
+                if (node == null)
+                    continue;
+
+                var hashValue = Hash(node.Key);
+                _hashMap[hashValue] = node;
+                _size++;
+            }
+        }
+    }
+
 }
--- Tree.cs
+++ Tree.cs
@@ -2,11 +2,466 @@
 
 namespace DataStructures
 {
-    public class BinarySearchTree {}
+    public class BinarySearchTreeNode
+    {
+        public int Key { get; set; }
+        public int Value { get; set; }
 
-    public class SegmentTree {}
+        public BinarySearchTreeNode Left { get; set; }
+        public BinarySearchTreeNode Right { get; set; }
 
-    public class AVLTree {}
+        public BinarySearchTreeNode(int key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+    public class BinarySearchTree
+    {
+        private BinarySearchTreeNode Root { get; set; }
+
+        public BinarySearchTree() {}
+
+        public void Insert(int key, int val)
+        {
+            Root = InsertInternal(Root, key, val);
+        }
+
+        private BinarySearchTreeNode InsertInternal(BinarySearchTreeNode node, int key, int value)
+        {
+            if (node == null || node.Key == key)
+                return new BinarySearchTreeNode(key, value);
+
+            if (key > node.Key)
+                node.Right = InsertInternal(node.Right, key, value);
+            else
+                node.Left = InsertInternal(node.Left, key, value);
+
+            return node;
+        }
+
+        public int Get(int key)
+        {
+            return GetInternal(Root, key);
+        }
+
+        private int GetInternal(BinarySearchTreeNode node, int key)
+        {
+            if (node == null)
+                return -1;
+
+            if (key > node.Key)
+                return GetInternal(node.Right, key);
+            else if (key < node.Key)
+                return GetInternal(node.Left, key);
+            else
+                return node.Value;
+        }
+
+        public int GetMin()
+        {
+            BinarySearchTreeNode currentNode = Root;
+
+            if (currentNode == null)
+                return -1;
+
+            while (currentNode.Left != null)
+                currentNode = currentNode.Left;
+
+            return currentNode.Value;
+        }
+
+        public int GetMax()
+        {
+            BinarySearchTreeNode currentNode = Root;
+
+            if (currentNode == null)
+                return -1;
+
+            while (currentNode.Right != null)
+                currentNode = currentNode.Right;
+
+            return currentNode.Value;
+        }
+
+        public void Remove(int key)
+        {
+            Root = RemoveInternal(Root, key);
+        }
+
+        private BinarySearchTreeNode RemoveInternal(BinarySearchTreeNode node, int key)
+        {
+            if (node == null)
+                return null;
+
+            if (key > node.Key)
+                node.Right = RemoveInternal(node.Right, key);
+            else if (key < node.Key)
+                node.Left = RemoveInternal(node.Left, key);
+            else
+            {
+                if (node.Left == null)
+                    return node.Right;
+                else if (node.Right == null)
+                    return node.Left;
+                else
+                {
+                    var minNode = FindMin(node.Right);
+
+                    node.Key = minNode.Key;
+                    node.Value = minNode.Value;
+                    node.Right = RemoveInternal(node.Right, node.Key);
+                }
+            }
+
+            return node;
+        }
+
+        private BinarySearchTreeNode FindMin(BinarySearchTreeNode node)
+        {
+            while (node?.Left != null)
+                node = node.Left;
+
+            return node;
+        }
+
+        public List<int> GetInorderKeys()
+        {
+            var keys = new List<int>();
+            GetInOrderKeyInternal(keys, Root);
+
+            return keys;
+        }
+
+        private List<int> GetInOrderKeyInternal(List<int> keys, BinarySearchTreeNode node)
+        {
+            if (node == null)
+                return keys;
+
+            GetInOrderKeyInternal(keys, node.Left);
+            keys.Add(node.Key);
+            GetInOrderKeyInternal(keys, node.Right);
+
+            return keys;
+        }
+    }
+
+    public class SegmentNode
+    {
+        public int Value { get; set; }
+
+        public int LeftIndex { get; set; }
+        public int RightIndex { get; set; }
+
+        public SegmentNode LeftNode { get; set; }
+        public SegmentNode RightNode { get; set; }
+
+        public SegmentNode(int value, int leftIndex, int rightIndex)
+        {
+            Value = value;
+            LeftIndex = leftIndex;
+            RightIndex = rightIndex;
+        }
+    }
+    public class SegmentTree
+    {
+
+        private SegmentNode _segmentNodeRoot;
+
+        public SegmentTree(int[] nums)
+        {
+            _segmentNodeRoot = GenerateSegmentTree(nums, 0, nums.Length - 1);
+        }
+
+        public void update(int index, int val)
+        {
+            UpdateInternal(index, val, _segmentNodeRoot);
+        }
+
+        private void UpdateInternal(int targetIndex, int newValue, SegmentNode currentNode)
+        {
+            if (currentNode.LeftIndex == currentNode.RightIndex)
+            {
+                currentNode.Value = newValue;
+                return;
+            }
+
+            int midIndex = currentNode.LeftIndex + ((currentNode.RightIndex - currentNode.LeftIndex) / 2);
+
+            if (targetIndex <= midIndex)
+                UpdateInternal(targetIndex, newValue, currentNode.LeftNode);
+            else
+                UpdateInternal(targetIndex, newValue, currentNode.RightNode);
+
+            currentNode.Value = currentNode.LeftNode?.Value + currentNode.RightNode?.Value ?? 0;
+        }
+
+        public int query(int L, int R)
+        {
+            Console.WriteLine("Start " + L + " " + R);
+            return QueryInternal(L, R, _segmentNodeRoot);
+        }
+
+        private int QueryInternal(int leftIndex, int rightIndex, SegmentNode currentNode)
+        {
+            if (currentNode.LeftIndex == leftIndex && currentNode.RightIndex == rightIndex)
+                return currentNode.Value;
+
+            int midIndex = currentNode.LeftIndex + ((currentNode.RightIndex - currentNode.LeftIndex) / 2);
+
+            if (rightIndex <= midIndex)
+                return QueryInternal(leftIndex, rightIndex, currentNode.LeftNode);
+            else if (leftIndex > midIndex)
+                return QueryInternal(leftIndex, rightIndex, currentNode.RightNode);
+            else
+                return QueryInternal(leftIndex, midIndex, currentNode.LeftNode) +
+                       QueryInternal(midIndex + 1, rightIndex, currentNode.RightNode);
+        }
+
+        private SegmentNode GenerateSegmentTree(int[] nums, int leftIndex, int rightIndex)
+        {
+            if (leftIndex == rightIndex)
+                return new SegmentNode(nums[leftIndex], leftIndex, rightIndex);
+
+            int midIndex = leftIndex + ((rightIndex - leftIndex) / 2);
+
+            var segmentNode = new SegmentNode(0, leftIndex, rightIndex);
+
+            segmentNode.LeftNode = GenerateSegmentTree(nums, leftIndex, midIndex);
+            segmentNode.RightNode = GenerateSegmentTree(nums, midIndex + 1, rightIndex);
+
+            segmentNode.Value = segmentNode.LeftNode?.Value + segmentNode.RightNode?.Value ?? 0;
+
+            return segmentNode;
+        }
+    }
+
+    public class AVLNode
+    {
+        public int BalanceFactor { get; set; }
+        public string Value { get; set; }
+        public int Height { get; set; }
+        public AVLNode? Left;
+        public AVLNode? Right;
+
+        public AVLNode(string value)
+        {
+            Value = value;
+        }
+    }
+    public class AVLTree
+    {
+
+
+        public AVLNode? Root { get; set; }
+        private int nodeCount = 0;
+
+        public int GetHeight()
+        {
+            if (Root == null)
+                return 0;
+
+            return Root.Height;
+        }
+
+        public int Size() => nodeCount;
+
+        public bool IsEmpty() => Size() == 0;
+
+        public bool Contains(string value) => InternalContains(Root, value);
+
+        private bool InternalContains(AVLNode? node, string value)
+        {
+            if (node == null)
+                return false;
+
+            int comparisonResult = value.CompareTo(node.Value);
+
+            if (comparisonResult < 0)
+                return InternalContains(node.Left, value);
+
+            if (comparisonResult > 0)
+                return InternalContains(node.Right, value);
+
+            return true;
+        }
+
+        public bool Insert(string? value)
+        {
+            if (value == null)
+                return false;
+
+            if (!InternalContains(Root, value))
+            {
+                Root = InternalInsert(Root, value);
+                nodeCount++;
+                return true;
+            }
+
+            return false;
+        }
+
+        private AVLNode InternalInsert(AVLNode? node, string value)
+        {
+            if (node == null)
+                return new AVLNode(value);
+
+            int comparisonResult = value.CompareTo(node.Value);
+
+            if (comparisonResult < 0)
+                node.Left = InternalInsert(node.Left, value);
+
+            if (comparisonResult > 0)
+                node.Right = InternalInsert(node.Right, value);
+
+            Update(node);
+
+            return Balance(node);
+        }
+
+        private void Update(AVLNode node)
+        {
+            int leftNodeHeight = (node.Left == null) ? -1 : node.Left.Height;
+            int rightNodeHeight = (node.Right == null) ? -1 : node.Right.Height;
+
+            node.Height = 1 + Math.Max(leftNodeHeight, rightNodeHeight);
+
+            node.BalanceFactor = rightNodeHeight - leftNodeHeight;
+        }
+
+        private AVLNode Balance(AVLNode node)
+        {
+            if (node.BalanceFactor == -2)
+            {
+                if (node.Left.BalanceFactor <= 0)
+                    return LeftLeftCase(node);
+                else
+                    return LeftRightCase(node);
+            }
+            else if (node.BalanceFactor == 2)
+            {
+                if (node.Right.BalanceFactor >= 0)
+                    return RightRightCase(node);
+                else
+                    return RightLeftCase(node);
+            }
+
+            return node;
+        }
+
+
+        private AVLNode LeftLeftCase(AVLNode node) => RightRotation(node);
+
+        private AVLNode LeftRightCase(AVLNode node)
+        {
+            node.Left = LeftRotation(node.Left);
+            return LeftLeftCase(node);
+        }
+
+        private AVLNode RightRightCase(AVLNode node) => LeftRotation(node);
+
+        private AVLNode RightLeftCase(AVLNode node)
+        {
+            node.Right = RightRotation(node.Right);
+            return RightRightCase(node);
+        }
+
+        private AVLNode LeftRotation(AVLNode node)
+        {
+            AVLNode newParent = node.Right;
+            node.Right = newParent.Left;
+            newParent.Left = node;
+
+            Update(node);
+            Update(newParent);
+
+            return newParent;
+        }
+
+        private AVLNode RightRotation(AVLNode node)
+        {
+            AVLNode newParent = node.Left;
+            node.Left = newParent.Right;
+            newParent.Right = node;
+
+            Update(node);
+            Update(newParent);
+
+            return newParent;
+        }
+
+        public bool Remove(string value)
+        {
+            if (value == null)
+                return false;
+
+            if (InternalContains(Root, value))
+            {
+                Root = InternalRemove(Root, value);
+                nodeCount--;
+                return true;
+            }
+
+            return false;
+        }
+
+        private AVLNode? InternalRemove(AVLNode? node, string value)
+        {
+            if (node == null)
+                return null;
+
+            int comparisonResult = value.CompareTo(node.Value);
+
+            if (comparisonResult < 0)
+                node.Left = InternalRemove(node.Left, value);
+            else if (comparisonResult > 0)
+                node.Right = InternalRemove(node.Right, value);
+            else
+            {
+                if (node.Left == null)
+                    return node.Right;
+                else if (node.Right == null)
+                    return node.Left;
+                else
+                {
+                    if (node.Left.Height > node.Right.Height)
+                    {
+                        string successorValue = FindMax(node.Left);
+                        node.Value = successorValue;
+
+                        node.Left = InternalRemove(node.Left, successorValue);
+                    }
+                    else
+                    {
+                        string successorValue = FindMin(node.Right);
+                        node.Value = successorValue;
+
+                        node.Right = InternalRemove(node.Right, successorValue);
+                    }
+                }
+            }
+
+            Update(node);
+
+            return Balance(node);
+        }
+
+        private string FindMin(AVLNode node)
+        {
+            while (node.Left != null)
+                node = node.Left;
+
+            return node.Value;
+        }
+
+        private string FindMax(AVLNode node)
+        {
+            while (node.Right != null)
+                node = node.Right;
+
+            return node.Value;
+        }
+    }
 
     public class FenwickTree
     {
@@ -85,8 +540,4 @@ namespace DataStructures
 
         public void Set(int i, long value) => Add(i, value - Sum(i, i));
     }
-
-    public class BinarySearchTreeSet {}
-
-    public class BinarySearachTreeMap {}
 }
