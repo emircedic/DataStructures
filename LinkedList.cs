0f0c0669ed3489ce3aee567915d75f8e0e070460
Emir <45953377+emircedic@users.noreply.github.com>
2025-06-18 16:03:39 +0200

    Update LinkedList.cs

--- LinkedList.cs
+++ LinkedList.cs
@@ -120,14 +120,15 @@
             return values;
         }
     }
+     
 
     public class DoublyLinkedListNode
     {
         public int Value { get; set; }
-        public DoublyLinkedListNode Prev { get; set; }
-        public DoublyLinkedListNode Next { get; set; }
-
-        public DoublyLinkedListNode(int value)
+        public Node Previous { get; set; }
+        public Node Next { get; set; }
+    
+        public Node (int value)
         {
             Value = value;
         }
@@ -135,62 +136,121 @@
 
     public class DoublyLinkedList
     {
-        DoublyLinkedListNode head;
-        DoublyLinkedListNode tail;
-
-        public DoublyLinkedList()
+        DoublyLinkedListNode _headNode;
+        DoublyLinkedListNode _tailNode;
+    
+        int _nodeCount;
+    
+        public DoublyLinkedList() {}
+    
+        public int Get(int index)
         {
-            head = new DoublyLinkedListNode(-1);
-            tail = new DoublyLinkedListNode(-1);
-            head.Next = tail;
-            tail.Prev = head;
+            if (index >= _nodeCount)
+                return -1;
+    
+            var currentNode = _headNode;
+            for (int i = 0; i < index; i++)
+                currentNode = currentNode.Next;
+    
+            return currentNode.Value;    
         }
-
-        public void InsertFront(int val)
+        
+        public void AddAtHead(int val)
         {
-            DoublyLinkedListNode newNode = new DoublyLinkedListNode(val)
+            var newNode = new DoublyLinkedListNode(val);
+    
+            if (_headNode == null)
             {
-                Prev = head,
-                Next = head.Next
-            };
-
-            head.Next.Prev = newNode;
-            head.Next = newNode;
-        }
-
-        public void InsertEnd(int val)
-        {
-            DoublyLinkedListNode newNode = new DoublyLinkedListNode(val)
-            {
-                Next = tail,
-                Prev = tail.Prev
-            };
-
-            tail.Prev.Next = newNode;
-            tail.Prev = newNode;
-        }
-
-        public void RemoveFront()
-        {
-            head.Next.Next.Prev = head;
-            head.Next = head.Next.Next;
-        }
-
-        public void RemoveEnd()
-        {
-            tail.Prev.Prev.Next = tail;
-            tail.Prev = tail.Prev.Prev;
-        }
-
-        public void Print()
-        {
-            DoublyLinkedListNode curr = head.Next;
-            while (curr != tail)
-            {
-                Console.WriteLine(curr.Value + " -> ");
-                curr = curr.Next;
+                _headNode = newNode;
+                _tailNode = newNode;
             }
-            Console.WriteLine("");
+            {
+                newNode.Next = _headNode;
+                _headNode.Previous = newNode;
+    
+                _headNode = newNode;
+            }
+    
+            _nodeCount += 1;    
+        }
+        
+        public void AddAtTail(int val)
+        {
+            var newNode = new DoublyLinkedListNode(val);
+    
+            if (_tailNode == null)
+            {
+                _tailNode = newNode;
+                _headNode = newNode;
+            }
+            else
+            {
+                newNode.Previous = _tailNode;
+                _tailNode.Next = newNode;
+    
+                _tailNode = newNode;
+            }
+    
+            _nodeCount += 1;
+        }
+        
+        public void AddAtIndex(int index, int val)
+        {
+            if (index > _nodeCount)
+                return;
+    
+            if (index == 0)
+                AddAtHead(val);
+            else if (index == _nodeCount)
+                AddAtTail(val);
+            else
+            {
+                var currentNode = _headNode;
+                for (int i = 0; i < index; i++)
+                    currentNode = currentNode.Next;
+    
+                var newNode = new DoublyLinkedListNode(val);
+                newNode.Next = currentNode;
+                newNode.Previous = currentNode.Previous;
+    
+                currentNode.Previous.Next = newNode;
+                currentNode.Previous = newNode;
+    
+                _nodeCount += 1;    
+            }
+        }
+        
+        public void DeleteAtIndex(int index)
+        {
+            if (index >= _nodeCount)
+                return;
+    
+            if (_nodeCount == 1)
+            {
+                _headNode = null;
+                _tailNode = null;
+            }
+            else if (index == 0)
+            {
+                _headNode.Next.Previous = null;
+                _headNode = _headNode.Next;
+            }
+            else if (index == _nodeCount - 1)
+            {
+                _tailNode.Previous.Next = null;
+                _tailNode = _tailNode.Previous;
+            }
+            else
+            {
+                var currentNode = _headNode;
+                for (int i = 0; i < index; i++)
+                    currentNode = currentNode.Next;
+    
+                currentNode.Previous.Next = currentNode.Next;
+                currentNode.Next.Previous = currentNode.Previous;
+            }
+    
+            _nodeCount -= 1;    
         }
     }
 }
